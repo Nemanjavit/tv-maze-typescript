8356022993a83262bef91f2148dd361685fbcc02
Nemanjavit <nemanja@butchershop.co>
2023-05-29 14:30:14 -0600

    fix scroll width calculation

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { Container, Row } from "react-bootstrap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
@@ -11,20 +11,33 @@ const HomeHero = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const rowRef = useRef<HTMLDivElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const [scrollDistance, setScrollDistance] = useState<number>(0);
   const allShows = useAppSelector((state) => getTopRatedShows(state));
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
     setIsLoaded(true);
+    return () => {
+      setIsLoaded(false);
+    };
   }, []);
 
   useEffect(() => {
-    if (!rowRef.current) return;
+    requestAnimationFrame(() => {
+      const scrollWidth =
+        document.querySelector(".card-slider")?.scrollWidth || 0;
 
-    const scrollWidth = rowRef.current.scrollWidth || 0;
-    const containerWidth = containerRef.current?.clientWidth || 0;
-    const scrollDistance = scrollWidth - containerWidth;
-    console.log(scrollDistance);
+      const containerWidth =
+        document.querySelector(".card-slider")?.getBoundingClientRect().width ||
+        0;
+
+      setScrollDistance(scrollWidth - containerWidth);
+    });
+  }, [isLoaded]);
+
+  useEffect(() => {
+    if (!rowRef.current || !containerRef.current) return;
+
     let ctx = gsap.context(() => {
       gsap.fromTo(
         rowRef.current,
@@ -37,20 +50,19 @@ const HomeHero = () => {
             end: "+=4000",
             pin: true,
             scrub: 0.8,
+            invalidateOnRefresh: true,
           },
         }
       );
     });
 
     return () => ctx.revert();
-  }, [isLoaded]);
-
-  if (!allShows) return null;
+  }, [isLoaded, scrollDistance]);
 
   return (
     <Container ref={containerRef}>
       <div className="container-padding homeHero">
-        <Row ref={rowRef} className="flex-nowrap">
+        <Row ref={rowRef} className="flex-nowrap card-slider">
           {allShows.map((show) => (
             <Card key={show.id} show={show} />
           ))}
